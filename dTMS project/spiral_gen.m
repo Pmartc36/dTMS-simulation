246f636ad5492e0234de7f7e525faeee92fe84bf
jayakumarPawan <120random.things@gmail.com>
2019-02-04 20:52:03 -0500

    added oreintation param

--- dTMS project/spiral_gen.m
+++ dTMS project/spiral_gen.m
@@ -1,8 +1,7 @@
-function [x,y,z] = spiral_gen(x0,y0,z0,points,u,r0,h)
+function [x,y,z] = spiral_gen(x0,y0,z0,points,u,r0,h,orientation)
 %points is number of points
 %u is a constant that determines the distance between wraps
 %r0 is a constant that determines the smallest part of the spiral
-%n is number loops
 %h is height
 %omega is a constant that determines how many windings the spiral will have
 %phi0 is a constant that deterimines which angle the spiral starts
@@ -11,8 +10,22 @@ omega = h/(points*2);
 r = r0 + u * t;
 phi = -omega*t;
 
-z = r .* cos(phi)+z0;
-y = r .* sin(phi)+y0;
-x = omega*t+x0;
+if(orientation == "x")
+    z = r .* cos(phi)+z0;
+    y = r .* sin(phi)+y0;
+    x = omega*t+x0;
 end
 
+if(orientation == "y")
+    z = r .* cos(phi)+z0;
+    x = r .* sin(phi)+y0;
+    y = omega*t+x0;
+end
+
+if(orientation == "z")
+    y = r .* cos(phi)+z0;
+    x = r .* sin(phi)+y0;
+    z = omega*t+x0;
+end
+
+end
